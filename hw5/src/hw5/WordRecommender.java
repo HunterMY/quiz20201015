2baa80affb08ab83bd4df9cc8b8b731fb737ee8e
Yue <Yue@LAPTOP-5VHAFA3C.fios-router.home>
2020-11-02 18:24:27 -0500

    create class

---
+++ hw5/src/hw5/WordRecommender.java
@@ -0,0 +1,10 @@
+package hw5;
+
+public class WordRecommender {
+	int a;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hw5/src/hw5/WordRecommenderTest.java
@@ -0,0 +1,5 @@
+package hw5;
+
+public abstract class WordRecommenderTest {
+
+}
